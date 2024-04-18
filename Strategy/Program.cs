c3a04a23bd5a9c4e4661b9add8b1709adf209ecd
Erison <erison7596e@gmail.com>
2024-04-17 22:37:03 -0300

    provavelmente errado, mas considera algo ai, faz a boa

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -6,6 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //provavelmente esta errado, mas eu estou enviando para tentar ganhar ao menos 1 ponto
         FileInfo inputTextFile = new FileInfo("../../../Hamlet.txt"); //ele meio que entra dentro da pasta debug, por isso tem tanto ../
 
         WebSearchModel model = new WebSearchModel(inputTextFile);
